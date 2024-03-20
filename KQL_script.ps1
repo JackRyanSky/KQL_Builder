41f7dcf05817db8f04ebcae70566b6b0af045475
JackRyanSky <164089878+JackRyanSky@users.noreply.github.com>
2024-03-20 19:10:20 -0400

    added a little better input validation among other things

--- KQL_script.ps1
+++ KQL_script.ps1
@@ -5,10 +5,10 @@ function main {
         $query = ""
         $userinput = (read-host -Prompt "Welcome to the EPIC DSL Builder`nPick an Option below:`n`n1) REGEX`n2) LIST OF VALUES`n3) IP list from file`n4) EXIT`n`n") #welcome terminal -> $userinput
         if ($userinput -eq "1") {
-            write-host "you chose option 1 REGEX`n" 
+            write-host "you chose option 1 REGEX`n"
             $field = (read-host "What is the field name?")
             $regex = (read-host "Write your regex")
-            return $(regexFunction $field $regex).TrimEnd(",") #todo: ensure this works
+            return $(regexFunction $field $regex).TrimEnd(",")
             }
  
         elseif ($userinput -eq "2") {
@@ -24,7 +24,7 @@ function main {
                         write-host "Stopping and returning filter"
                         break
                         }
-                    $ip = (read-host "STOP or write IP here")
+                    $IP = (read-host "STOP or write IP here")
                     if ($srcdstboth -eq "1" -or $srcdstboth -eq "3"){
                         #write-host "`nYou chose option 1, Source (or both)"
                         $query = "$query$(lineCheck $IP $sourcefield)"
@@ -33,14 +33,10 @@ function main {
                         #write-host "`nYou chose option 2, Destination (or both)"
                         $query = "$query$(lineCheck $IP $destinationField)"
                         }
-                    else {
-                        write-host "`nUnexpected input found, sanitizing IP and continuing"
-                        $ip = ""
-                        continue :labelA
-                        }
                     }
                 $query = $query.TrimEnd(",")
                 $query = "$query$(endANDOR)"
+                write-host "$query"
                 return $query
                 }
             elseif ($ipsorregex -eq "2"){
@@ -108,7 +104,7 @@ function main {
                 }
             $query = $query.trimend(",")
             $query = "$query$(endANDOR)"
-            return $query
+            write-host $query
             }
         elseif ($userinput -eq "4") {
             write-host "`nExiting"
@@ -121,10 +117,16 @@ function main {
         }
     }
  
-function CIDRtoRange {
+function CIDRtoRange { # in: cidr IP out: network and broadcast address
     param(
-        [Parameter(ValueFromPipeline, Mandatory, HelpMessage = 'Please enter subnet in CIDR notation (#.#.#.#/#)')][string] $CidrIP
+        [Parameter(ValueFromPipeline, Mandatory, HelpMessage = 'Please enter subnet in CIDR notation (#.#.#.#/#)')]
+        [string] $CidrIP
         )
+    if ( $cidrIP -notmatch "^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}[\/][\d][\d]?$"){
+        write-error "CIDR notation not inputted correctly. Please try again"
+        recall "CIDRtoRange"
+        break
+        }
     $IP, $CIDR = $CidrIP.Split("/")
     $binOctArr = $IP.Split(".")
     $i = 0
@@ -143,9 +145,26 @@ function CIDRtoRange {
     $broadcastIP = ([IPaddress]"$([convert]::toint64("$broadcastBinIP",2))").IPAddressToString # converting from binary IP to '.' deliminated IP.
     return @($networkIP, $broadcastIP) #return an array containing the network and broadcast IP's
     }
+
+function recall(){ # in: function name out: call function again
+    param(
+        [parameter()]
+        [string] $ret
+        )
+    &$ret
+    }
  
- 
-function lineCheck($line, $field){
+function lineCheck(){ # in: line and field     out: range function
+    param(
+        [parameter(Mandatory, Helpmessage = 'No Line provided')]
+        [string] $line,
+        [parameter(Mandatory, Helpmessage = 'source.ip | destination.ip')]
+        [string] $field
+        )
+    if ($line -notmatch "^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}" -or $field -notmatch "^(source\.ip|destination\.ip)$"){
+        Write-Error "IP formatted incorrectly or no field selected. Please Try again"
+        recall "lineCheck"
+        }
     if ( $line -match ","){
         $a = $line.Split(",")
         return $(rangeFunction $field $a[0] $a[1])
@@ -155,32 +174,52 @@ function lineCheck($line, $field){
         return $(rangeFunction $field $netAndBroad[0] $netAndBroad[1])
         }
     else{
-        return $(matchIPFunction $field $line)
+        return $(matchFunction $field $line)
         }
     }
  
-function regexFunction($field, $regex){
+function regexFunction(){
+    param(
+        [parameter(Mandatory, Helpmessage = '')]
+        [string] $field,
+        [parameter(Mandatory)]
+        [string] $regex
+        )
     $query = "{`"regexp`":{`"$field`":`"$regex`"}}," # use .TrimEnd(",") method if you only need one
     return $query
     }
  
  
-function rangeFunction($srcdst, $start, $end){
+function rangeFunction(){
+    param(
+        [parameter(Mandatory)]
+        [string] $srcdst,
+        [parameter(Mandatory)]
+        [string] $start,
+        [parameter(Mandatory)]
+        [string] $end
+        )
+    if ($srcdst -notmatch "^(source\.ip|destination\.ip)$" -or $start -notmatch "((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}" -or $end -notmatch "((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}"){
+        Write-Error "Something went wrong"
+        recall "rangeFunction"
+        break
+        }
     $query = "{`"range`":{`"$srcdst`":{`"lt`":`"$end`",`"gte`":`"$start`"}}}," # use .TrimEnd(",") method if you only need one
     return $query
     }
  
 function matchFunction(){
-    $field = (read-host "What is the field name?")
-    $phrase = (read-host "What is the exact phrase that you are trying to match?")
-    $query = ("{`"match_phrase`":{`"$field`":`"$phrase`"}},")
+    [cmdletbinding()]
+    param(
+        [parameter()]
+        [string] $field = $(read-host "What is the field name?"),
+        [parameter()]
+        [string] $value = $(read-host "What is the exact phrase that you are trying to match?")
+        )
+    $query = ("{`"match_phrase`":{`"$field`":`"$value`"}},")
     return $query
     }
- 
-function matchIPFunction ($fieldName, $valueName){
-    $query = "{`"match_phrase`":{`"$fieldName`":`"$valueName`"}},"
-    return $query
-    }
+
  
 function addAND(){
     return "{`"bool`":{`"must`":["
